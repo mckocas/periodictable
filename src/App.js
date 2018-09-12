b88287d0167a1787f0235c8677448cf6b3afcf4a
mckocas <kocasmehmetcan@gmail.com>
2018-09-12 16:32:29 +0300

    adding lanthanum and actinium(elements) to lanthanides and actinides selections

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       colors: [],
     }
   }
+
   componentDidMount(){
     this.getColors();
   }
@@ -21,9 +22,11 @@ class App extends Component {
   demoMethod = () => {
     this.clearSettings();
   }
+
   setElement(el){
     this.setState({element: el});
   }
+
   getPeriods(){
     const p1 = this.state.elements.period1;
     const p2 = this.state.elements.period2;
@@ -37,6 +40,7 @@ class App extends Component {
     const periods = [p1,p2,p3,p4,p5,p6,p7,lanthanides,actinides];
     return periods;
   }
+
   getColors(){
     const periods = this.getPeriods();
     const colors = [];
@@ -47,80 +51,99 @@ class App extends Component {
     });
     this.setState({colors: colors});
   }
-clearSettings(){
-    const periods = this.getPeriods();
-    const colors = this.state.colors;
-    let i = 0;
-    periods.forEach((prd) =>{
-      prd.forEach((elm) =>{
-        let change = elm;
-        change.background = colors[i];
-        i++;
-        this.setState({change});
+
+  clearSettings(){
+      const periods = this.getPeriods();
+      const colors = this.state.colors;
+      let i = 0;
+      periods.forEach((prd) =>{
+        prd.forEach((elm) =>{
+          let change = elm;
+          change.background = colors[i];
+          i++;
+          this.setState({change});
+        })
       })
-    })
-}
-
-colorizePeriodSelection(period){
-  const periods = this.getPeriods();
-  let periodNumber;
-
-  if(period === 'lanthanides'){
-    periodNumber = 7;
-  }
-  else if(period === 'actinides'){
-    periodNumber = 8;
-  }
-  else{
-    periodNumber = Number(period) - 1;
   }
 
-  if(period === 5){
-    this.state.elements.lanthanides.forEach(elm =>{
-      periods[5].push(elm);
-    });
-  }
-  else if(period === 6){
-    this.state.elements.actinides.forEach(elm =>{
-      periods[6].push(elm);
-    });
-  }
+  colorizePeriodSelection(period){
+    const periods = this.getPeriods();
+    const actinium = this.state.elements.period7[2];
+    const lanthanum = this.state.elements.period6[2];
+    let periodNumber;
 
-  for(let i = 0; i<periods.length;i++){
-    if(i === periodNumber){
-      continue;
+    if(period === 'lanthanides'){
+      periodNumber = 7;
+    }
+    else if(period === 'actinides'){
+      periodNumber = 8;
+    }
+    else{
+      periodNumber = Number(period) - 1;
     }
 
-    if(periodNumber === 5 && i === 7){
-      continue;
-    }
-    else if(periodNumber === 6 && i === 8){
-      continue;
-    } 
-
-    for(let k = 0; k<periods[i].length;k++){
-      let change = periods[i][k];
-      change.background= 'black';
-      this.setState({change});
-    }
-
-    }
     if(period === 5){
       this.state.elements.lanthanides.forEach(elm =>{
-        periods[5].pop(elm);
+        periods[5].push(elm);
       });
     }
     else if(period === 6){
       this.state.elements.actinides.forEach(elm =>{
-        periods[6].pop(elm);
+        periods[6].push(elm);
       });
     }
+    else if(period === 'lanthanides'){
+      periods[7].unshift(lanthanum);
+    }else if(period === 'actinides'){
+
+      periods[8].unshift(actinium);
+    }
+
+    for(let i = 0; i<periods.length;i++){
+      if(i === periodNumber){
+        continue;
+      }
+
+      if(periodNumber === 5 && i === 7){
+        continue;
+      }
+      else if(periodNumber === 6 && i === 8){
+        continue;
+      }
+
+      for(let k = 0; k<periods[i].length;k++){
+        if(periodNumber === 7 && i === 5 && k === 2){
+          continue;
+        }else if(periodNumber === 8 && i === 6 && k === 2){
+          continue;
+        }
+        let change = periods[i][k];
+        change.background= 'black';
+        this.setState({change});
+      }
+
+      }
+      if(period === 5){
+        this.state.elements.lanthanides.forEach(elm =>{
+          periods[5].pop(elm);
+        });
+      }
+      else if(period === 6){
+        this.state.elements.actinides.forEach(elm =>{
+          periods[6].pop(elm);
+        });
+      }
+      else if(period === 'lanthanides'){
+        periods[7].shift(lanthanum);
+      }
+      else if(period === 'actinides'){
+        periods[8].shift(actinium);
+      }
   }
 
   setPeriodSelection(period){
     this.clearSettings();
     this.colorizePeriodSelection(period);
-
   }
 
   render() {
